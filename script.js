24a721fef7c91a619684429e6cc84d44955b3d08
Purvi <purvitrpathi28@gmail.com>
2025-11-25 16:41:43 +0530

    Adding Feature Layer

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+], function (Map, MapView, FeatureLayer) {
   const myMap = new Map({
     basemap: "topo",
   });
@@ -6,5 +10,14 @@ require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
   const myView = new MapView({
     container: "viewDiv",
     map: myMap,
+  });
+  const FeatureLayer_1 = new FeatureLayer({
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/Landscape_Trees/FeatureServer/0",
+  });
+
+  myMap.add(FeatureLayer_1);
+  FeatureLayer_1.when(() => {
+    console.log("Layer loaded");
+    myView.goTo(FeatureLayer_1.fullExtent);
   });
 });
