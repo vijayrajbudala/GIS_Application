926a37cf6a4e6383119a59bb1f6ac5896aa4df25
Aswin <aswiniswain121@gmail.com>
2025-11-26 11:33:49 +0530

    added the basemap toggle

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ require([
   "esri/layers/FeatureLayer",
   "esri/widgets/ScaleBar",
   "esri/widgets/Bookmarks",
-    "esri/widgets/Expand",
-    "esri/widgets/Measurement",
-  "esri/widgets/FeatureTable"
-
-], function (Map, MapView, FeatureLayer, ScaleBar, Bookmarks, Expand, Measurement, FeatureTable) {
+  "esri/widgets/Expand",
+  "esri/widgets/Measurement",
+  "esri/widgets/FeatureTable",
+  "esri/widgets/BasemapToggle"
+], function (
+  Map, MapView, FeatureLayer, ScaleBar, Bookmarks, Expand, Measurement, FeatureTable, BasemapToggle
+) {
 
   const myMap = new Map({
     basemap: "topo",
@@ -28,10 +30,10 @@ require([
   fLayer.when(() => {
     myView.goTo(fLayer.fullExtent);
   });
-  
+
   const scaleBar = new ScaleBar({
     view: myView,
-    unit: "metric" 
+    unit: "metric"
   });
   myView.ui.add(scaleBar, "bottom-left");
 
@@ -44,19 +46,26 @@ require([
     content: bookmarks,
     expanded: false
   });
+  myView.ui.add(bkExpand, "top-right");
 
-    myView.ui.add(bkExpand, "top-right");
+  const measurement = new Measurement({
+    view: myView,
+    activeTool: "distance"
+  });
+  myView.ui.add(measurement, "top-left");
 
-    const measurement = new Measurement({
-        view: myView,
-        activeTool: "distance"
-    });
-    myView.ui.add(measurement, "top-left");
+  const featureTable = new FeatureTable({
+    view: myView,
+    layer: fLayer,
+    container: "tableDiv"
+  });
 
-    const featureTable = new FeatureTable({
-        view: myView,
-        layer: fLayer,
-        container: "tableDiv"
-    });
+ 
+  const basemapToggle = new BasemapToggle({
+    view: myView,
+    nextBasemap: "satellite"
+  });
+
+  myView.ui.add(basemapToggle, "bottom-right");
 
 });
